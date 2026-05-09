0b8f52074637be2e2c13e05fa310b8ef6cc64768
vndmea <vndmea@gmail.com>
2026-05-09 21:40:45 +0800

    feat(ckeditor5): add adapter types

---
+++ packages/ckeditor5/src/types.ts
@@ -0,0 +1,52 @@
+import type { KityEditorAssets } from '@formulax/kity-runtime';
+
+export interface FormulaXPayload {
+  latex: string;
+  html?: string;
+}
+
+export interface FormulaXCKEditor5Options {
+  buttonName?: string;
+  toolbarText?: string;
+  tooltip?: string;
+  formulaClassName?: string;
+  formulaAttributeName?: string;
+  modal?: {
+    title?: string;
+    insertText?: string;
+    updateText?: string;
+    cancelText?: string;
+    closeOnBackdrop?: boolean;
+  };
+  editor?: {
+    height?: number | string;
+    autofocus?: boolean;
+    assets?: Partial<KityEditorAssets>;
+    render?: {
+      fontsize?: number;
+    };
+  };
+}
+
+export interface RequiredFormulaXCKEditor5Options {
+  buttonName: string;
+  toolbarText: string;
+  tooltip: string;
+  formulaClassName: string;
+  formulaAttributeName: string;
+  modal: {
+    title: string;
+    insertText: string;
+    updateText: string;
+    cancelText: string;
+    closeOnBackdrop: boolean;
+  };
+  editor: {
+    height: number | string;
+    autofocus: boolean;
+    assets: Partial<KityEditorAssets>;
+    render: {
+      fontsize: number;
+    };
+  };
+}
